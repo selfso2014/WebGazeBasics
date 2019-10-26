19830294534571b173f2ed6fe27f19a00a9bad4d
selfso <selfso@visual.camp>
2019-10-26 09:06:12 +0900

    Initial Commit

---
+++ WebGazeBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebGazeBasics
+//
+//  Created by Steve on 26/10/2019.
+//  Copyright © 2019 Visualcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
